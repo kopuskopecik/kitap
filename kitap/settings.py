13bc088e515be7989ed8bca42da7df5bb1b544a7
kopus <kopuskopecik@gmail.com>
2019-06-17 15:52:39 +0300

    Proje ilk hali

--- kitap/settings.py
+++ kitap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4adlwdpf6krz)0lgbk)$_231ef5q)fc81g*z)aetr+ghi4(k(7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["eturnuva.pythonanywhere.com"]
 
 
 # Application definition
