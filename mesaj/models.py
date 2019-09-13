fedc9e9d1ffb4997b65d1afd9ceb31398fa8d0a3
kopus <kopuskopecik@gmail.com>
2019-09-13 18:58:36 +0300

    ckeditor eklendi

--- mesaj/models.py
+++ mesaj/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from accounts.models import User
 
+from ckeditor.fields import RichTextField
+
 # Create your models here.
 class Entry(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name = "süperkullanıcı", blank = True, null = True)
-	mesaj = models.TextField()
+	mesaj = RichTextField()
 	resim = models.ImageField(blank=True, null = True)
 	publishing_date = models.DateTimeField(verbose_name="yayımlanma_tarihi",auto_now_add=True)
 	
